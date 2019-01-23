5a38521bc5cfe6b63c2aaff847711051117f2bfb
poepping <matt.poepping@gmail.com>
2019-01-23 15:20:29 -0700

    part one done

---
+++ day24/code.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+
+import re
+from recordclass import recordclass
+from pprint import pprint 
+import math
+import copy
+
+MyStruct = recordclass('system', 'id systemType weakTo immuneTo units hitPoints attack attackType init effectivePower selected defendingGroup')
+
+inputFile = "test_input.txt"
+inputFile = "input.txt"
+
+
+def damage(a,b):
+    if a.attackType in b.weakTo:
+        mult = 2
+    else:
+        mult = 1
+
+    if a.attackType in b.immuneTo:
+        mult = 0
+    
+    #print("{} {} {}".format(a.id, b.id, a.effectivePower * mult) )
+    return a.effectivePower * mult 
+
+# read in input file
+data=[]
+currentType = False
+id_counter = {'immune': 1, 'infection': 1}
+for line in map(lambda x: x.strip(), open(inputFile)):
+  if line == 'Immune System:':
+    currentType = 'immune'
+  elif line == 'Infection:':
+    currentType = 'infection'
+  else:
+    m = re.match(r'(.*) units each with (.*) hit points *(\(.*\))* with an attack that does (.*) (.*) damage at initiative (.*)$', line)
+    if m:
+      weakTo = []
+      immuneTo=[]
+      
+      stats = m.group(3)
+      if stats:
+          for s in map(lambda x: x.strip(), stats[1:-1].split(';')):
+              if s[0] == 'w':
+                  weakTo =list(map(lambda x: x.strip(), s[7:].split(',')))
+              if s[0] == 'i':
+                  immuneTo  = list(map(lambda x: x.strip(), s[9:].split(',')))
+      
+      s = MyStruct(systemType=currentType, units=int(m.group(1)),
+                   hitPoints=int(m.group(2)), attack=int(m.group(4)),
+                   attackType=m.group(5), init=int(m.group(6)),
+                   effectivePower=0, weakTo=weakTo, immuneTo=immuneTo,
+                   selected=False, id=0, defendingGroup=-1)
+      s.effectivePower = s.units * s.attack 
+      s.id = id_counter[currentType]
+      id_counter[currentType] +=1
+      data.append(s)
+
+data = sorted(data, key=lambda x: (x.effectivePower, x.init), reverse=True)
+#pprint(data)
+#pprint( damage(data[1], data[2] ))
+
+def check_done(d):
+    z = []
+    for a in d:
+       z.append(a.systemType)
+    return len(set(z)) == 2
+
+while check_done(data):
+    print("----")
+    for d in data:
+        d.selected = False
+        
+    # Selection Phase
+    # priority of selection starts with effective power, and if there is a tie,
+    # then the initiative is used
+    for player in sorted(data, key=lambda x: (x.effectivePower, x.init), reverse=True):
+        enemy = list(filter( lambda x: x.systemType!=player.systemType and x.selected == False, data))
+        selected = list(sorted(enemy, key=lambda y: (damage(player,y),y.effectivePower,y.init), reverse=True))
+        if len(selected) > 0:
+            selected = selected[0]
+        else:
+            continue
+        selected.selected = True
+        player.defendingGroup = selected.id
+        #print("type: {} id {} will deal {} damage to type:{} id:{}".format(player.systemType, player.id, damage(player, selected), selected.systemType, selected.id))
+        #print("---")
+
+    # Attack Phase
+    for player in sorted(data, key=lambda x: x.init, reverse=True):
+        if player.defendingGroup == -1:
+            continue
+        enemy = list(filter(lambda x: x.systemType != player.systemType and x.id == player.defendingGroup, data))[0]
+        d = damage( player, enemy)
+        units = math.floor( d / enemy.hitPoints  )
+        if units > enemy.units:
+            units = enemy.units 
+        print("{} group {} attacks defending group {}, killing {} units".format(player.systemType, player.id, player.defendingGroup, units))
+        enemy.units -= units 
+        enemy.effectivePower = enemy.units * enemy.attack
+
+    # remove all dead units
+    newData = []
+    for d in data:
+        if d.units > 0: 
+            d.selected = False
+            d.effectivePower = d.units * d.attack
+            d.defendingGroup = -1
+            newData.append(d)
+    data = copy.deepcopy(newData)
+    #pprint(data)
+
+pprint(data)
+
+print ( sum([d.units for d in data] ) )
